b750307e310c4a241a8ce183907ee026dc68d2bc
Michelle Lee <mlee092@michellsmacbook.myfiosgateway.com>
2020-03-31 19:55:03 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,134 @@
 import React, { Component } from 'react';
 import './stylesheets/App.css'
 import { Segment } from 'semantic-ui-react';
-
+import WestworldMap from './components/WestworldMap';
+import Headquarters from './components/Headquarters';
+import { Log } from './services/Log'
 
 class App extends Component {
 
-  // As you go through the components given you'll see a lot of functional components.
-  // But feel free to change them to whatever you want.
-  // It's up to you whether they should be stateful or not.
+  state = {
+    areas: [],
+    allHosts: [],
+    activeHosts: [],
+    coldHosts: [],
+    currentHost: null,
+    logs: [],
+    massAction: "activate"
+  }
+
+  checkAreaRoom = (area) =>{
+    let areaObj = this.state.areas.find(a => a.name === area)
+    let hostsInHere = this.state.activeHosts.filter(host=> host.area === areaObj.name)
+    return (hostsInHere.length + 1 <= areaObj.limit)
+  }
+
+  createLog(msg){
+    let newLogs = this.state.logs
+    switch (msg.split(" ")[0]){
+      case "Moved":
+        newLogs.unshift(Log.notify(msg))
+        break;
+      case "Too":
+        newLogs.unshift(Log.error(msg))
+        break;
+      case "Activated":
+        newLogs.unshift(Log.warn(msg))
+        break;
+      case "Decomissioned":
+        newLogs.unshift(Log.notify(msg))
+        break;
+      case "Decomissioning":
+        newLogs.unshift(Log.notify(msg))
+        break;
+      case "Activating":
+        newLogs.unshift(Log.warn(msg))
+        break;
+      default:
+        break;
+    } 
+    this.setState({
+      logs: newLogs
+    })
+  }
+
+  removeAndReplace = (newHost, logMsg) => {
+    if(this.checkAreaRoom(newHost.area)){
+      let newHosts = this.state.allHosts.filter(host => host.id !== newHost.id)
+      newHosts.push(newHost)
+
+      this.createLog(logMsg)
+    
+      this.setState({
+        allHosts: [...newHosts],
+        activeHosts: newHosts.filter(host => host.active),
+        coldHosts: newHosts.filter(host => !host.active),
+        currentHost: newHost
+      })
+
+    }else{
+      console.log('ya cant do that')
+      this.createLog(`Too many hosts - cannot add ${newHost.name} to ${newHost.area}`)
+    }
+    
+  }
+
+  swapHost = (host) =>{
+    this.setState({
+      currentHost: host
+    })
+  }
+
+  changeAllStatus = () =>{
+    let newHosts = [...this.state.allHosts]
+    let newCurrentHost
+    let action = this.state.massAction === "activate" ? true : false
+
+    if(this.state.currentHost){
+      newCurrentHost = {...this.state.currentHost}
+      newCurrentHost.active = action
+    }else{
+      newCurrentHost = null
+    }
+    newHosts.forEach(host => host.active = action)
+
+    this.createLog(action? "Activating all hosts" : "Decomissioning all hosts")
+
+    this.setState({
+      allHosts: [...newHosts],
+      activeHosts: newHosts.filter(host => host.active),
+      coldHosts: newHosts.filter(host => !host.active),
+      currentHost: newCurrentHost,
+      massAction: action ? "decomission" : "activate" 
+    })
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:4000/hosts")
+    .then(resp => resp.json())
+    .then(data => {
+      console.log("\n\nHOSTS...setting host data after mount\n\n")
+      this.setState({
+        allHosts: data,
+        coldHosts: data
+      })
+    })
+
+    fetch("http://localhost:4000/areas")
+    .then(resp => resp.json())
+    .then(data => {
+      console.log("\n\nAREAS...setting area data after WWMap mount\n\n")
+      this.setState({
+        areas: data
+      })
+    })
+  }
 
   render(){
     return (
       <Segment id='app'>
-        {/* What components should go here? Check out Checkpoint 1 of the Readme if you're confused */}
+        <WestworldMap swapHost={this.swapHost} currentHost={this.state.currentHost} hosts={this.state.activeHosts} areas={this.state.areas}/>
+        <Headquarters swapHost={this.swapHost} currentHost={this.state.currentHost} hosts={this.state.coldHosts} removeAndReplace={this.removeAndReplace} numHosts={this.state.allHosts.length} logs={this.state.logs} changeAllStatus={this.changeAllStatus} massAction={this.state.massAction}/>
       </Segment>
     )
   }
--- src/components/LogPanel.js
+++ src/components/LogPanel.js
@@ -2,36 +2,35 @@ import React from 'react'
 import { Segment, Button } from 'semantic-ui-react';
 import { Log } from '../services/Log'
 
-const LogPanel = () => {
+const LogPanel = (props) => {
 
-  const dummyLogs = () => {
-    // This is just to show you how this should work. But where should the log data actually get stored?
-    // And where should we be creating logs in the first place?
-    // Use the Log Service class (located in: 'src/services/Log') we've created anywhere you like.
-    // Just remember to import it
+  // logs.unshift(Log.warn("This is an example of a warn log"))
+  // logs.unshift(Log.notify("This is an example of a notify log"))
+  // logs.unshift(Log.error("This is an example of an error log"))
 
-    let logs = []
+  const getButtonValue = () =>{
+    if(props.massAction === "activate" && props.coldHosts > 0){
+      return "ACTIVATE ALL"
+    }else{
+      return "DECOMISSION ALL"
+    }
+  }
 
-    logs.unshift(Log.warn("This is an example of a warn log"))
-    logs.unshift(Log.notify("This is an example of a notify log"))
-    logs.unshift(Log.error("This is an example of an error log"))
-
-    return logs
+  const getButtonColor = () => {
+    return getButtonValue() === "ACTIVATE ALL" ? "red" : "green"
   }
 
   return(
     <Segment className="HQComps" id="logPanel">
       <pre>
-        {dummyLogs().map((log, i) => <p key={i} className={log.type}>{log.msg}</p>)}
+        {props.logs.map((log, i) => <p key={i} className={log.type}>{log.msg}</p>)}
       </pre>
-      
-      {/* Button below is the Activate All/Decommisssion All button */}
+  
       <Button
+        onClick={props.changeAllStatus}
         fluid
-        color={"red"}
-        {/* This isn't always going to be the same color...*/}
-        content={"ACTIVATE ALL"}
-        {/* Should the button always read "ACTIVATE ALL"? When should it read "DECOMMISSION ALL"? */}
+        content={getButtonValue()} 
+        color={getButtonColor()} 
       />
     </Segment>
   )
